ca436a5e2327d2173a95f3dc6111f02b7d95bece
Samuel Andrés <samuel5848@gmail.com>
2024-06-04 19:50:42 -0300

    : minimal config - no patches

--- config.h
+++ config.h
@@ -1,13 +1,43 @@
 /* See LICENSE file for copyright and license details. */
 
-/* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
+/* -- Constants -- */
+/* commands */
+#define TERMINAL 	    "st"
+#define BROWSER 	    "qutebrowser"
+#define BACKGROUND 	    "bg"
+#define MUSIC 		    "ncspot"
+#define EMOJI_PICKER        "emojipick"
+#define CALCULATOR 	    "bc"
+#define LAUNCHER            "dmenu_run"
+#define LOCKER              "slock"
+/* scratchpads */
+#define SPTERM 		    "spterm"
+#define SPCALC 		    "spcalc"
+#define SPMUSIC 	    "spmusic"
+#define XEVTITLE 	    "Event Tester"
+/* X window classes */
+#define TERMCLASS           "St"
+#define FMCLASS             "Ranger"
+/* key aliases */
+#define MODKEY              Mod4Mask /* SUPER */
+/* fonts */
+#define FONT                "monospace:pixelsize=12"
+#define TERM_FONT           "monospace:pixelsize=12"
+#define ICON_FONT           "Twemoji:pixelsize=12:antialias=true:autohint=true"
+
+/* -- Appearance -- */
+static unsigned int borderpx                = 3;   /* border pixel of windows */
+static unsigned int snap                    = 32;  /* snap pixel */
+static int showbar                          = 1;   /* 0 means no bar */
+static int topbar                           = 1;   /* 0 means bottom bar */
+static const unsigned int systraypinning    = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft     = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing    = 2;   /* systray spacing */
+static const int systraypinningfailfirst    = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray                = 1;   /* 0 means no systray */
+static const char *fonts[]                        = { FONT, ICON_FONT };
+static const char dmenufont[]               = FONT;
+static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
@@ -18,7 +48,7 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
-/* tagging */
+/* -- Tagging -- */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -27,14 +57,13 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ TERMCLASS,  NULL,       NULL,       0,            0,           -1 }, /* every terminal  */
 };
 
-/* layout(s) */
+/* -- Layout(s) -- */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -45,7 +74,6 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,41 +88,111 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+/* -- Bindings -- */
+#include <X11/XF86keysym.h> /* Provides XF86XK_* keys (multimedia keys)  */
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    /* modifier                     key             function            argument */
+
+    /* Move the focus to the next/previous/master window in the stack*/
+    { MODKEY,                       XK_j,           focusstack,         {.i = +1 } },
+    { MODKEY,                       XK_k,           focusstack,         {.i = -1 } },
+    { MODKEY,                       XK_v,           focusstack,         {.i = 0  } },
+
+    /* Show only the windows with the selected tag */
+    { MODKEY,                       XK_1,           view,               {.ui = 1 << 0} },
+    { MODKEY,                       XK_2,           view,               {.ui = 1 << 1} },
+    { MODKEY,                       XK_3,           view,               {.ui = 1 << 2} },
+    { MODKEY,                       XK_4,           view,               {.ui = 1 << 3} },
+    { MODKEY,                       XK_5,           view,               {.ui = 1 << 4} },
+    { MODKEY,                       XK_6,           view,               {.ui = 1 << 5} },
+    { MODKEY,                       XK_7,           view,               {.ui = 1 << 6} },
+    { MODKEY,                       XK_8,           view,               {.ui = 1 << 7} },
+    { MODKEY,                       XK_9,           view,               {.ui = 1 << 8} },
+    { MODKEY,                       XK_0,           view,               {.ui = ~0    } },
+    { MODKEY,                       XK_Tab,         view,               {0           } }, /* previously selected tag */
+
+    /* Replaces the tag for the focused window */
+    { MODKEY|ShiftMask,             XK_1,           tag,                {.ui = 1 << 0} },
+    { MODKEY|ShiftMask,             XK_2,           tag,                {.ui = 1 << 1} },
+    { MODKEY|ShiftMask,             XK_3,           tag,                {.ui = 1 << 2} },
+    { MODKEY|ShiftMask,             XK_4,           tag,                {.ui = 1 << 3} },
+    { MODKEY|ShiftMask,             XK_5,           tag,                {.ui = 1 << 4} },
+    { MODKEY|ShiftMask,             XK_6,           tag,                {.ui = 1 << 5} },
+    { MODKEY|ShiftMask,             XK_7,           tag,                {.ui = 1 << 6} },
+    { MODKEY|ShiftMask,             XK_8,           tag,                {.ui = 1 << 7} },
+    { MODKEY|ShiftMask,             XK_9,           tag,                {.ui = 1 << 8} },
+    { MODKEY|ShiftMask,             XK_0,           tag,                {.ui = ~0    } },
+
+    /* Show/hide windows with the selected tag */
+    { MODKEY|ControlMask,           XK_1,           toggleview,         {.ui = 1 << 0} },
+    { MODKEY|ControlMask,           XK_2,           toggleview,         {.ui = 1 << 1} },
+    { MODKEY|ControlMask,           XK_3,           toggleview,         {.ui = 1 << 2} },
+    { MODKEY|ControlMask,           XK_4,           toggleview,         {.ui = 1 << 3} },
+    { MODKEY|ControlMask,           XK_5,           toggleview,         {.ui = 1 << 4} },
+    { MODKEY|ControlMask,           XK_6,           toggleview,         {.ui = 1 << 5} },
+    { MODKEY|ControlMask,           XK_7,           toggleview,         {.ui = 1 << 6} },
+    { MODKEY|ControlMask,           XK_8,           toggleview,         {.ui = 1 << 7} },
+    { MODKEY|ControlMask,           XK_9,           toggleview,         {.ui = 1 << 8} },
+
+    /* Add/remove the selected tag to the focused window */
+    { MODKEY|ControlMask|ShiftMask, XK_1,           toggletag,          {.ui = 1 << 0} },
+    { MODKEY|ControlMask|ShiftMask, XK_2,           toggletag,          {.ui = 1 << 1} },
+    { MODKEY|ControlMask|ShiftMask, XK_3,           toggletag,          {.ui = 1 << 2} },
+    { MODKEY|ControlMask|ShiftMask, XK_4,           toggletag,          {.ui = 1 << 3} },
+    { MODKEY|ControlMask|ShiftMask, XK_5,           toggletag,          {.ui = 1 << 4} },
+    { MODKEY|ControlMask|ShiftMask, XK_6,           toggletag,          {.ui = 1 << 5} },
+    { MODKEY|ControlMask|ShiftMask, XK_7,           toggletag,          {.ui = 1 << 6} },
+    { MODKEY|ControlMask|ShiftMask, XK_8,           toggletag,          {.ui = 1 << 7} },
+    { MODKEY|ControlMask|ShiftMask, XK_9,           toggletag,          {.ui = 1 << 8} },
+
+    /* Move the focused window to the top of the stack (MASTER) */
+    { MODKEY,                       XK_Return,      zoom,               {0           } },
+
+    /* Increase the number of masters by 1 */
+    { MODKEY,                       XK_o,           incnmaster,         {.i = +1     } },
+
+    /* Decrease the number of masters by 1 */
+    { MODKEY|ShiftMask,             XK_o,           incnmaster,         {.i = -1     } },
+
+    /* Kill the focused window */
+    { MODKEY|ShiftMask,             XK_BackSpace,   killclient,         {0           } },
+
+    /* Change the current layout */
+    { MODKEY,                       XK_t,           setlayout,          {.v = &layouts[0]} }, /* tile */
+    { MODKEY|ShiftMask,             XK_f,           setlayout,          {.v = &layouts[1]} }, /* floating */
+    { MODKEY|ShiftMask,             XK_u,           setlayout,          {.v = &layouts[2]} }, /* monocle */
+
+    /* Increase/decrease master's size */
+    { MODKEY,                       XK_h,           setmfact,           {.f = -0.05} },
+    { MODKEY,                       XK_l,           setmfact,           {.f = +0.05} },
+
+    /* Show/hide status bar */
+    { MODKEY,                       XK_b,           togglebar,          {0         } },
+
+    /* Handle multiple monitors */
+    { MODKEY,                       XK_Left,        focusmon,           {.i = -1   } },
+    { MODKEY|ShiftMask,             XK_Left,        tagmon,             {.i = -1   } },
+    { MODKEY,                       XK_Right,       focusmon,           {.i = +1   } },
+    { MODKEY|ShiftMask,             XK_Right,       tagmon,             {.i = +1   } },
+
+    /* Move a window in/out of the stack (float) */
+    { MODKEY|ShiftMask,             XK_space,       togglefloating,     {0         } },
+
+    /* Launch commands */
+    { MODKEY|ShiftMask,             XK_Return,                spawn,              {.v = termcmd} },
+    { MODKEY,                       XK_w,                     spawn,              {.v = (const char*[]){ BROWSER, NULL } } },
+    { MODKEY,                       XK_e,                     spawn,              {.v = (const char*[]){ EMOJI_PICKER, NULL } } },
+    { MODKEY,                       XK_p,                     spawn,              {.v = (const char*[]){ LAUNCHER, NULL } } },
+    { MODKEY|ShiftMask,             XK_comma,                 spawn,              {.v = (const char*[]){ LOCKER, NULL } } },
+    { MODKEY|Mod1Mask,              XK_s,                     spawn,              SHCMD("scrot -s ~/Pictures/Screenshots/%b%d::%H%M%S.png")},
+    { 0,                            XF86XK_MonBrightnessUp,   spawn,              SHCMD("brightnessctl -q set 5%+")},
+    { 0,                            XF86XK_MonBrightnessDown, spawn,              SHCMD("brightnessctl -q set 5%-")},
+    { 0,                            XF86XK_AudioRaiseVolume,  spawn,              SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%+")},
+    { 0,                            XF86XK_AudioLowerVolume,  spawn,              SHCMD("wpctl set-volume @DEFAULT_SINK@ 5%-")},
+    { 0,                            XF86XK_AudioMute,         spawn,              SHCMD("wpctl set-mute @DEFAULT_SINK@ toggle")},
+
+    /* End X session */
+    { MODKEY|ShiftMask,             XK_q,           quit,               {0         } },
 };
 
 /* button definitions */
